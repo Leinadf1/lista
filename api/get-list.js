ceac352a4029f84ad42e1ca85a85955f8eb64c31
Leinadf1 <162797896+Leinadf1@users.noreply.github.com>
2026-07-28 16:24:46 +0200

    Update get-list.js

--- api/get-list.js
+++ api/get-list.js
@@ -266,4 +266,4 @@ export default async function handler(req, res) {
         console.error(error);
         res.status(500).json({ error: "Errore caricamento liste" });
     }
-                }
+}
